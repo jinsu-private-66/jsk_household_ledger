624966753bff1367dc2cbb309f549cc03f045925
jinsu-private-66 <jinsudevelop66@gamil.com>
2021-10-31 18:43:21 +0900

    common파일 등록

---
+++ src/components/common/navbar.vue
@@ -0,0 +1,99 @@
+<template>
+<div>
+  <v-navigation-drawer permanent>
+    <v-list>
+      <v-list-item class="px-2">
+        <h2><Strong>TokyoKane</Strong></h2>
+      </v-list-item>
+
+      <v-list-item link>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Jinsu
+          </v-list-item-title>
+          <v-list-item-subtitle>jinsu6688@gmail.com</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list
+      nav
+      dense
+    >
+      <v-list-item >
+        <v-list-item-icon>
+          <v-icon>mdi-folder</v-icon>
+        </v-list-item-icon>
+        <router-link to="/" class="navbar__link">HOME</router-link>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-star</v-icon>
+        </v-list-item-icon>
+          <router-link to="/withdrawal/new" class="navbar__link">지출기록</router-link>
+      </v-list-item>
+
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-account-multiple</v-icon>
+        </v-list-item-icon>
+        <router-link :to="currentYearMonthToCalendarUrl()" class="navbar__link">지출내역(캘린더)</router-link>
+      </v-list-item>
+
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-star</v-icon>
+        </v-list-item-icon>
+        <router-link to="/withdrawal?page=1" class="navbar__link">지출내역(전체)</router-link>
+      </v-list-item>
+
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-star</v-icon>
+        </v-list-item-icon>
+        <router-link :to="currentYearMonthToMonthUrl()" class="navbar__link">지출내역(월)</router-link>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    currentYearMonthToMonthUrl () {
+      const now = new Date();
+
+      return `/withdrawal/month?year=${now.getFullYear()}&month=${now.getMonth() + 1}`;
+    },
+    currentYearMonthToCalendarUrl () {
+      const now = new Date();
+
+      return `/withdrawal/calendar?year=${now.getFullYear()}&month=${now.getMonth() + 1}`;
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  width: 100%;
+  height: 50px;
+  display: flex;
+  &__links {
+    width: 100%;
+    display: flex;
+    text-decoration: none;
+    justify-content: space-between;
+  }
+  &__left-wrapper,
+  &__right-wrapper {
+    a {
+      margin-left: 15px;
+    }
+  }
+}
+</style>
---
+++ src/components/common/the_date_picker.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-menu
+    v-model="showDataMenu"
+    :close-on-content-click="false"
+    transition="scale-transition"
+    offset-y
+    min-width="auto"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-text-field
+        v-model="date"
+        label="Date Picker"
+        prepend-icon="mdi-calendar"
+        clearable
+        readonly
+        v-bind="attrs"
+        v-on="on"
+        @click:clear="$emit('input', '')"
+      />
+    </template>
+    <v-date-picker
+      v-model="date"
+      @input="showDataMenu = false"
+      @change="change"
+    />
+  </v-menu>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      date: '',
+      showDataMenu: false
+    }
+  },
+  methods: {
+    change (date) {
+      this.$emit('input', date)
+    }
+  }
+}
+</script>
